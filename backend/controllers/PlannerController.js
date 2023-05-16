c4d477bb4051568d68d3524215b5305d7be5c55a
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-05-16 11:35:19 +0200

    planner post valid

--- backend/controllers/PlannerController.js
+++ backend/controllers/PlannerController.js
@@ -6,8 +6,35 @@ const upload = require('../upload');
 router.post('/planner', upload.none(), async (req, res, next) => {
     try {
       const { fog, rain, moon, clouds, date, stars, constellations } = req.body;
+      if(fog < 0 || fog > 100){
+        res.status(400).json({
+          status_code: 400,
+          message: 'Fog must be between 0 and 100',
+          error: true
+        });
+        return;
+      }
+      const planner = new Planner({
+        fog: fog,
+        rain: rain,
+        moon: moon,
+        clouds: clouds,
+        nightSkyDate: date,
+        constellationList: constellations,
+        starsList: stars
+      });
+      await planner.save();
+      res.status(200).json({
+        status_code: 200,
+        planner: planner,
+        error: false
+    });
     } catch (error) {
-      res.status(500).json({ error: 'An error occurred while saving the planner data' });
+      res.status(500).json({
+        status_code: 500,
+        message: 'Planner not saved',
+        error: true
+    });
     }
 });  
 
